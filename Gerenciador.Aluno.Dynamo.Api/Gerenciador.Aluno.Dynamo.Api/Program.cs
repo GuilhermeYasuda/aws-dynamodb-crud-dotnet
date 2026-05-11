38787a24ffc0211441d6e4e974ca2175e1a22bea
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-05-11 18:37:59 -0300

    chore: integração com DynamoDB e documentação Scalar
    Adicionados pacotes AWSSDK e Scalar.AspNetCore ao projeto. Configurados serviços para uso do DynamoDB e leitura das configurações AWS via appsettings.json. Ajustado launchSettings para abrir navegador em "scalar/v1". Incluída seção "AWS" no appsettings.json para perfil e região.

--- Gerenciador.Aluno.Dynamo.Api/Gerenciador.Aluno.Dynamo.Api/Program.cs
+++ Gerenciador.Aluno.Dynamo.Api/Gerenciador.Aluno.Dynamo.Api/Program.cs
@@ -1,3 +1,7 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// carrega a configuração da AWS do arquivo appsettings.json para o ambiente de execução da aplicação
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+// registra o serviço do Amazon DynamoDB para ser injetado em outros componentes da aplicação para trabalhar com tabelas e itens
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+// registra o serviço do DynamoDBContext para ser injetado em outros componentes da aplicação para realizar operações de leitura e escrita em tabelas do DynamoDB usando uma abordagem orientada a objetos
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
