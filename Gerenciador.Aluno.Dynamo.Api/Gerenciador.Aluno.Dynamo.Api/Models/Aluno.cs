47bea47d4c1b96a00bb2691bbe61e8fdb07e4b80
Guilherme Harunobu Yasuda <haru_yasuda@hotmail.com>
2026-05-11 18:53:35 -0300

    feat: adicionar classe Aluno com mapeamento para DynamoDB
    Inclui a classe Aluno no namespace Gerenciador.Aluno.Dynamo.Api.Models, utilizando atributos do Amazon DynamoDB para mapear as propriedades Id, Nome, Sobrenome, Classe e Pais à tabela "alunos". Todas as propriedades são opcionais.

---
+++ Gerenciador.Aluno.Dynamo.Api/Gerenciador.Aluno.Dynamo.Api/Models/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Gerenciador.Aluno.Dynamo.Api.Models
+{
+    [DynamoDBTable("alunos")]
+    public class Aluno
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+        [DynamoDBProperty("nome")]
+        public string? Nome { get; set; }
+        [DynamoDBProperty("sobrenome")]
+        public string? Sobrenome { get; set; }
+        [DynamoDBProperty("classe")]
+        public int? Classe { get; set; }
+        [DynamoDBProperty("pais")]
+        public string? Pais { get; set; }
+    }
+}
